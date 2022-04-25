bc9726d9a12a1d3084179e6db26f598e2f998447
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-04-25 17:19:53 +1000

    format

--- duckdemo/main.py
+++ duckdemo/main.py
@@ -3,8 +3,12 @@
 # wget https://github.com/cwida/duckdb-data/releases/download/v1.0/taxi_2019_04.parquet
 import duckdb
 
-print(duckdb.query('''
+print(
+    duckdb.query(
+        """
 SELECT COUNT(*)
 FROM 'taxi_2019_04.parquet'
 WHERE pickup_at BETWEEN '2019-04-15' AND '2019-04-20'
-''').fetchall())
+"""
+    ).fetchall()
+)
