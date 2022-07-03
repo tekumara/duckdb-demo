6a377305ae81518c1974c60df57435e3446a7cf1
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2022-07-03 10:20:01 +0000

    feat: read parquet schema from s3

--- duckdemo/schema.py
+++ duckdemo/schema.py
@@ -1,7 +1,19 @@
+# import os
+
 import duckdb
 
 con = duckdb.connect()
 
-con.execute("SELECT * FROM parquet_schema('data/userdata1.parquet');")
+con.execute("INSTALL httpfs;")
+con.execute("LOAD httpfs;")
+# con.execute("SET s3_region='us-east-1';")
+# con.execute(f"SET s3_access_key_id='{os.getenv('AWS_ACCESS_KEY_ID')}';")
+# con.execute(f"SET s3_secret_access_key='{os.getenv('AWS_SECRET_ACCESS_KEY')}';")
+# con.execute(f"SET s3_session_token='{os.getenv('AWS_SESSION_TOKEN')}';")
+
+# will only fetch headers, not the whole parquet file
+con.execute(
+    "SELECT * FROM parquet_schema('s3://gbif-open-data-ap-southeast-2/occurrence/2022-07-01/occurrence.parquet/000000');"
+)
 for r in con.fetchall():
-    print(r)
+    print(",".join([str(e) for e in r]))
